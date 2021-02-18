af076f7cc07e48d5d824e63ecb593362f40a220e
souvenir718 <mongkey0@naver.com>
2021-02-18 09:11:21 +0900

    get movies query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,21 @@
 import React from 'react';
-export default () => <h1>Home</h1>;
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/client';
+
+const GET_MOVIES = gql`
+    {
+        movies {
+            id
+            medium_cover_image
+        }
+    }
+`;
+export default () => {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+    if (loading) {
+        return 'loading....';
+    }
+    if (data && data.movies) {
+        return data.movies.map((m) => <h1>{m.id}</h1>);
+    }
+};
